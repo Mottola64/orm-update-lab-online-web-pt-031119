652a7be905714944c5a520afc210d12b49605a44
Michael Mottola <mottola64@gmail.com>
2019-06-03 00:40:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,7 @@
 require_relative "../config/environment.rb"
 
 class Student
-attr_accessor :name, :grade
-attr_reader :id
+attr_accessor :name, :grade, :id
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
@@ -82,5 +81,5 @@ attr_reader :id
 
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
-  
+
 end
